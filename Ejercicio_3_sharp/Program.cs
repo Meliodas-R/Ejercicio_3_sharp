d7b2aeba0eea331ba8d8e0c3101aa93fb7c7ebdf
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-10-27 12:31:48 +0200

    Agregada la operación para calcular el consumo de gasolina y la muestra del resultado por pantalla.

--- Ejercicio_3_sharp/Program.cs
+++ Ejercicio_3_sharp/Program.cs
@@ -6,7 +6,14 @@ namespace Ejercicio_3_sharp
     {
         static void Main(string[] args)
         {
-            
+            decimal consumo;
+
+            Console.Write("Introduzca el número de kilómetros recorridos: ");
+            decimal kilometros = Convert.ToDecimal(Console.ReadLine());
+
+            consumo = kilometros / 30;
+            Console.WriteLine("\nEl rendimiento por litro de gasolina ronda los 30km/l");
+            Console.WriteLine(kilometros + " kilómetros suponen un consumo de: " + Math.Round(consumo, 2) + " litros de gasolina.");
         }
     }
 }
